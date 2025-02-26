92b09e158259fa36ff4f94d2e6fab49bbc353710
Ted Amaral <tedamaral@hotmail.com>
2025-02-26 06:24:53 -0300

    Cria função adicionar amigo e limpar nome

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+    if (nome == '') {
+        alert('Nome inválido');
+        limpaNome()
+        return;
+    }
+
+    if (listaDeAmigos.includes(nome)) {
+        alert('Nome já adicionado');
+        limpaNome()
+        return;
+    }
+    listaDeAmigos.push(nome);
+    console.log(listaDeAmigos);
+    limpaNome()
+}
+
+function limpaNome() {
+    let campo = document.querySelector('input');
+    campo.value = '';   
+}
